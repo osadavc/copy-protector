10edc551cc95610da76a18794a9d2fd229047dfd
osadavc <osadavidath@gmail.com>
2023-11-12 01:56:49 +0530

    🎉 Initial Commit

---
+++ src/options.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Options = () => {
+  return null;
+};
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>
+);
---
+++ src/popup.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Popup = () => {
+  return null;
+};
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>
+);
